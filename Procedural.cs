893f3448461d49ddfe352078bb1f45b6daf4889a
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2023-02-18 21:00:32 -0700

    improvement to using sin

--- Procedural.cs
+++ Procedural.cs
@@ -4,37 +4,30 @@ using UnityEngine;
 
 public class Procedural : MonoBehaviour
 {
-	public Transform prefab;
-	private int chunkSize = 40;
-  private int mainSurface = 10;
-  private float frequency = 0.2f;
-  private float amplitude = 4.0f;
+    public Transform prefab;
+    private int chunkSize = 90;
+    private int mainSurface = 35;
+    private float frequency = 0.1f;
+    private float amplitude = 6.0f;
 	
     void Start()
     {        
         for (int l = 0; l < chunkSize; l++)
         {
+          var x = l * 4;
           for (int w = 0; w < chunkSize; w++)
-          {
-            var rndBlock = Random.Range(0, 2);	
-            Debug.Log(rndBlock);
-            var x = l * 18;
-            var surface = mainSurface + Mathf.Sin((float)x + frequency) * amplitude;
+          {         	           
+            
+            var surface = mainSurface + Mathf.Sin((float)x * frequency) * amplitude;                   
+            //Debug.Log(surface);
 
             for (int h = 0; h < chunkSize; h++)
-            {              
+            {     
               if (h < surface){
                 Instantiate(prefab, new Vector3(l * 1F, h * 1F, w * 1F), Quaternion.identity);
               }
             }
           }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-	
+    }	
 }
