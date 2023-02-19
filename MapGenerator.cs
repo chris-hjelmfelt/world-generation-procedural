6650dedbe9ec3804311afccd1f7f7adc4fccd1dc
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2023-02-19 14:14:25 -0700

    update map dynamically

--- MapGenerator.cs
+++ MapGenerator.cs
@@ -8,12 +8,23 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
 
-    void Start()
-    { 
+    public void createMap() {
         float[,] noiseMap = PerlinNoise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
 
         // draw to screen
         MapDisplay display = FindObjectOfType<MapDisplay> ();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    void Start()
+    { 
+        createMap();
     }    
+
+    // Update the map when values in the inspector are changed
+    private void OnValidate() 
+    {
+        Debug.Log("changed");
+        createMap();
+    }
 }
