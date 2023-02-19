aa8343c6ef317873c6d4d1dc6c8eeae990bcb91e
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2023-02-18 20:30:16 -0700

    using sin to form the surface

--- Procedural.cs
+++ Procedural.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class Procedural : MonoBehaviour
 {
 	public Transform prefab;
-	public int chunkSize = 20;
+	private int chunkSize = 40;
+  private int mainSurface = 10;
+  private float frequency = 0.2f;
+  private float amplitude = 4.0f;
 	
-    // Start is called before the first frame update
     void Start()
     {        
-        for (int h = 0; h < chunkSize; h++)
+        for (int l = 0; l < chunkSize; l++)
         {
           for (int w = 0; w < chunkSize; w++)
           {
-            for (int l = 0; l < chunkSize; l++)
-            {
-				var rnd = Random.Range(0, 2);	
-				Debug.Log(rnd);
-				if (rnd == 1){
-					Instantiate(prefab, new Vector3(l * 1F, h * 1F, w * 1F), Quaternion.identity);
-				}
+            var rndBlock = Random.Range(0, 2);	
+            Debug.Log(rndBlock);
+            var x = l * 18;
+            var surface = mainSurface + Mathf.Sin((float)x + frequency) * amplitude;
+
+            for (int h = 0; h < chunkSize; h++)
+            {              
+              if (h < surface){
+                Instantiate(prefab, new Vector3(l * 1F, h * 1F, w * 1F), Quaternion.identity);
+              }
             }
           }
         }
